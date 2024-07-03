dea27962bfdbccae5999309b391d7394514ef8b8
Rodrigo Bergenthal <rodrigo_bergenthak@live.com>
2024-07-03 07:20:30 -0300

    adicionando concurrent

--- gruntfile.js
+++ gruntfile.js
@@ -25,6 +25,9 @@ module.exports = function (grunt) {
                     'main2.css': 'main.scss' 
                 }
             }
+        },
+        concurrent:{
+            target: ['olagrunt','less', 'sass','tarefaDemorada']
         }
     });
 
@@ -35,9 +38,16 @@ module.exports = function (grunt) {
             done();
         }, 3000);
     });
-
+    grunt.registerTask('tarefaDemorada', function () {
+        const done = this.async();
+        setTimeout(function () {
+            console.log('olá Grunt');
+            done();
+        }, 3000);
+    });
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.loadNpmTasks('grunt-concurrent');
 
-    grunt.registerTask('default', ['less', 'sass']);
+    grunt.registerTask('default', ['concurrent']);
 };

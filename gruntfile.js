36e3fefa415207cc16f065f85cd9484faead2c9a
Rodrigo Bergenthal <rodrigo_bergenthak@live.com>
2024-07-03 19:30:16 -0300

    att

--- gruntfile.js
+++ gruntfile.js
@@ -1,6 +1,8 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        
+        // Compila arquivos LESS
         less: {
             development: {
                 files: {
@@ -16,17 +18,88 @@ module.exports = function (grunt) {
                 }
             }
         },
-        watch:{
-            less:{
-                files:['src/styles/**/*.less'],
-                tasks:['less:development'],
+        
+        // Monitora mudanças em arquivos e executa tarefas
+        watch: {
+            less: {
+                files: ['src/styles/**/*.less'],
+                tasks: ['less:development'],
+            },
+            html: {
+                files: ['src/index.html'],
+                tasks: ['replace:dev']
             }
-        }
+        },
+        
+        // Substitui placeholders por caminhos reais
+        replace: {
+            dev: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDEREÇO_DO_CSS',
+                            replacement: "./styles/main.css"
+                        },
+                        {
+                            match: 'ENDEREÇO_DO_JS',
+                            replacement: '../src/scripts/main.js'
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['src/index.html'],
+                        dest: 'dev/'
+                    }
+                ]
+            },
+            dist: {
+                options: {
+                    patterns: [
+                        {
+                            match: 'ENDEREÇO_DO_CSS',
+                            replacement: "./styles/main.min.css"
+                        }
+                    ]
+                },
+                files: [
+                    {
+                        expand: true,
+                        flatten: true,
+                        src: ['prebuild/index.html'],
+                        dest: 'dist/'
+                    }
+                ]
+            }
+        },
+        
+        // Minifica arquivos HTML
+        htmlmin: {
+            dist: {
+                options: {
+                    removeComments: true,
+                    collapseWhitespace: true,
+                },
+                files: {
+                    'prebuild/index.html': 'src/index.html'
+                }
+            }
+        },
+        
+        // Limpa diretórios/arquivos especificados
+        clean: ['prebuild']
     });
 
+    // Carrega plugins necessários
     grunt.loadNpmTasks('grunt-contrib-less');
     grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-replace');
+    grunt.loadNpmTasks('grunt-contrib-htmlmin');
+    grunt.loadNpmTasks('grunt-contrib-clean');
 
+    // Define tarefas padrão
     grunt.registerTask('default', ['watch']);
-    grunt.registerTask('build', ['less:production']);
+    grunt.registerTask('build', ['less:production', 'htmlmin:dist', 'replace:dist', 'clean']);
 };
